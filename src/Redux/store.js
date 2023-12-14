0704aa4540f6b16c1b5b227ecf9f05e512dca4c7
avinash <avinash.kumar70008@gmail.com>
2023-12-14 17:20:11 +0530

    changes

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,6 @@
-// store.js
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "./redux/reducer";
+import rootReducer from "./reducer";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
-import store from "./store";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
